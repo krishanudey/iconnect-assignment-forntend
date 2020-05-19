8f7ad26adc2be7aecf069811a432f325f1a9b4cd
Krishanu Dey <krishanudey99@gmail.com>
2020-05-19 12:01:58 +0530

    Adds api service with login and register methods

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { AuthService } from "./auth.service";
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ApiService {
+  private apiUrl: string;
+  constructor(private http: HttpClient, private authService: AuthService) {
+    this.apiUrl = `http://${environment.apiUrl}/v1`;
+  }
+
+  public login(email: string, password: string) {
+    return this.http
+      .post<any>(`${this.apiUrl}/auth/login`, { email, password })
+      .pipe(
+        map((user) => {
+          this.authService.updateToken(user.token);
+          this.authService.updateUser(user.user);
+          return user;
+        })
+      );
+  }
+
+  public register(name: string, email: string, password: string) {
+    return this.http
+      .post<any>(`${this.apiUrl}/auth/register`, { name, email, password })
+      .pipe(
+        map((user) => {
+          this.authService.updateToken(user.token);
+          this.authService.updateUser(user.user);
+          return user;
+        })
+      );
+  }
+}
