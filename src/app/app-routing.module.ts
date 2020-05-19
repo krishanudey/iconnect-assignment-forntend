5d320c56ff854fd324eac243c91d7604e9984ea9
Krishanu Dey <krishanudey99@gmail.com>
2020-05-19 12:09:23 +0530

    Adds routes mapping

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { IsLoggedInGuard } from "./guards/is-logged-in.guard";
+import { LoginComponent } from "./components/login/login.component";
+import { IsAnonymousGuard } from "./guards/is-anonymous.guard";
+import { RegisterComponent } from "./components/register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: ProfileComponent,
+    canActivate: [IsLoggedInGuard],
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+    canActivate: [IsAnonymousGuard],
+  },
+  {
+    path: "register",
+    component: RegisterComponent,
+    canActivate: [IsAnonymousGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
